ad162230edeb20b24dfb96d63c8a6ffba0872dc7
Justin Smith <Justin-Smith@users.noreply.github.com>
2021-11-30 00:13:28 -0500

    Added value in wrong location

--- WishList/Models/ApplicationUser.cs
+++ WishList/Models/ApplicationUser.cs
@@ -5,8 +5,7 @@ namespace WishList.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        public virtual ApplicationUser User {get;set;}
-
+       
         public virtual ICollection<Item> Items {get;set;}
     }
 }
